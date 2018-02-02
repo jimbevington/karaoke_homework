f17f8fb103a9a3b5dee0c99a8cac8564bac6ac66
Jim Bevington <jim.bevington@hotmail.com>
2018-02-02 11:31:43 +0000

    changed add/remove guest test to use Guest objs, not strings

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -2,11 +2,13 @@ require('minitest/autorun')
 require('minitest/rg')
 
 require_relative('../room.rb')
+require_relative('../guest.rb')
 
 class TestRoom < MiniTest::Test
 
   def setup
     @room = Room.new("Golden Splendour")
+    @guest = Guest.new("the baby Jesus")
   end
 
   def test_room_has_name
@@ -22,14 +24,16 @@ class TestRoom < MiniTest::Test
   end
 
   def test_can_add_guests
-    @room.add_guest("the baby Jesus")
+    @room.add_guest(@guest)
     assert_equal(1, @room.guests.count)
   end
 
   def test_can_remove_guests
-    @room.add_guest("the baby Jesus")
-    @room.remove_guest("the baby Jesus")
+    @room.add_guest(@guest)
+    @room.remove_guest(@guest)
     assert_equal(0, @room.guests.count)
   end
+
+
 
 end
