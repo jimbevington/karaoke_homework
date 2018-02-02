652dfbc596ffc4b23198928eb8cfbe9be3fb601c
Jim Bevington <jim.bevington@hotmail.com>
2018-02-02 16:13:03 +0000

    added fave song parameter to guest.rb, plus test to guest_spec.rb

--- guest.rb
+++ guest.rb
@@ -1,10 +1,11 @@
 class Guest
 
-  attr_reader :name, :money
+  attr_reader :name, :money, :fave_song
 
-  def initialize(name, money)
+  def initialize(name, money, fave_song)
     @name = name
     @money = money
+    @fave_song = fave_song
   end
 
   def spend_money(amount)
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -6,7 +6,7 @@ require_relative('../guest.rb')
 class TestGuest < MiniTest::Test
 
   def setup
-    @guest = Guest.new("Father Christmas", 50)
+    @guest = Guest.new("Father Christmas", 50, "Maggot Brain")
   end
 
   def test_guest_has_name
@@ -21,4 +21,9 @@ class TestGuest < MiniTest::Test
     @guest.spend_money(25)
     assert_equal(25, @guest.money)
   end
+
+  def test_guest_has_fave_song
+    assert_equal("Maggot Brain", @guest.fave_song)
+  end
+  
 end
